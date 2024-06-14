8ae536ed00159ddd1fe788c3c7d3fc35fbee77e4
Naymur Rahman <naymurmn@gmail.com>
2024-06-14 18:14:44 +0600

    cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // app.use(cors());
 app.use(express.json());
